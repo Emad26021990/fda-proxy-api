1ae9c4a9eb93f8d901e2f98f2b9808798e77eabc
Emad26021990 <emad.h.aljarrah@gmail.com>
2025-12-28 18:16:47 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def get_drug(ndc: str):
 
     return {
         "brand_name": result.get("openfda", {}).get("brand_name", [""])[0],
-        "generic_name": result.get("openfda", {}).get("generic_name", [""])[0],
-        "manufacturer": result.get("openfda", {}).get("manufacturer_name", [""])[0],
-        "active_ingredients": result.get("active_ingredient", [""])[0],
-        "purpose": result.get("purpose", [""])[0],
-        "indications": result.get("indications_and_usage", [""])[0],
-        "do_not_use": result.get("warnings", [""])[0],
-        "dosage": result.get("dosage_and_administration", [""])[0]
+        #"generic_name": result.get("openfda", {}).get("generic_name", [""])[0],
+        #"manufacturer": result.get("openfda", {}).get("manufacturer_name", [""])[0],
+        #"active_ingredients": result.get("active_ingredient", [""])[0],
+        #"purpose": result.get("purpose", [""])[0],
+        #"indications": result.get("indications_and_usage", [""])[0],
+        #"do_not_use": result.get("warnings", [""])[0],
+        #"dosage": result.get("dosage_and_administration", [""])[0]
     }

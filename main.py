f1dc937ad1e05c8b8467e10dfe21eadc5258daf7
Emad26021990 <emad.h.aljarrah@gmail.com>
2025-12-28 17:16:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ app = FastAPI()
 @app.get("/drug")
 def get_drug(ndc: str):
     url = f"https://api.fda.gov/drug/label.json?search=openfda.product_ndc:{ndc}&limit=1"
-
-    response = requests.get(url, timeout=10)
-    data = response.json()
+    r = requests.get(url, timeout=10)
+    data = r.json()
 
     result = data["results"][0]
-    openfda = result.get("openfda", {})
 
     return {
-        "brand_name": openfda.get("brand_name", [""])[0],
-        "generic_name": openfda.get("generic_name", [""])[0],
-        "manufacturer": openfda.get("manufacturer_name", [""])[0],
-        "warnings": result.get("warnings", [""])[0]
+        "brand_name": result.get("openfda", {}).get("brand_name", [""])[0],
+        "generic_name": result.get("openfda", {}).get("generic_name", [""])[0],
+        "manufacturer": result.get("openfda", {}).get("manufacturer_name", [""])[0],
+        "active_ingredients": result.get("active_ingredient", [""])[0],
+        "purpose": result.get("purpose", [""])[0],
+        "indications": result.get("indications_and_usage", [""])[0],
+        "do_not_use": result.get("warnings", [""])[0],
+        "dosage": result.get("dosage_and_administration", [""])[0]
     }
